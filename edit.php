b1e9b456200ff3cc55cc67041c83bf4975b0c51d
Kevin diaz <40407751+Kevindm14@users.noreply.github.com>
2019-02-18 19:10:03 -0500

    Update edit.php

--- edit.php
+++ edit.php
@@ -3,22 +3,20 @@
 <?php
   include("conection.php");
   $id = $_GET['id'];
-
   $edit = "SELECT * FROM datos WHERE ID=$id";
   $result = mysqli_query($conection, $edit);
-
 ?>
 
 <div class="form-create">
     <form method="POST" action="update.php">
         <h2>Edit Product</h2>
         <?php while($list = mysqli_fetch_array($result)) {
-			echo "<label class='labels'>Name</label><br>";
-            echo "<input type='text' name='name' value='".$list['name']."'><br>";
-			echo "<label class='labels'>Price</label><br>";
+		echo "<label class='labels'>Name</label><br>";
+            	echo "<input type='text' name='name' value='".$list['name']."'><br>";
+		echo "<label class='labels'>Price</label><br>";
         	echo "<input type='text' name='price' value='".$list['price']."'>";
-			echo "<input type='hidden' name='id' value='".$id."'>";
-		}?>
-		<input type="submit" value=Save>
+		echo "<input type='hidden' name='id' value='".$id."'>";
+	}?>
+	<input type="submit" value=Save>
     </form>
 </div>
